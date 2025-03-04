c37bb6a265b45cbd47c9ecc958a2a09f4fd4cacb
MKaznacheev <kaznacheev.ma@gmail.com>
2025-03-05 00:01:20 +0400

    Add module-info.java for test module
    Signed-off-by: Matvei Kaznacheev <kaznacheev.ma@gmail.com>

---
+++ src/test/java/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Derivora Testing Data Support.
+ *
+ * Derivora Testing Data Support is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Derivora Testing Data Support is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Derivora Testing Data Support. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
+ */
+
+open module derivora.testing.data.support.test {
+    requires org.junit.jupiter.api;
+
+    requires derivora.util.kit;
+    requires derivora.testing.data.support;
+}
